fcc211c88a54adfd2eeb9031084199be8a3239ea
Lan Tian <xuyh0120@outlook.com>
2017-02-02 15:48:52 +0800

    Fix support for Chinese folders & Added hooks for other constructors of java.io.File

--- app/src/main/java/lantian/nolitter/XposedHook.java
+++ app/src/main/java/lantian/nolitter/XposedHook.java
@@ -9,6 +9,7 @@ import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.IXposedHookZygoteInit;
 import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XSharedPreferences;
+import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
@@ -42,12 +43,19 @@ public class XposedHook implements IXposedHookZygoteInit, IXposedHookLoadPackage
     public void initZygote(StartupParam startupParam) throws Throwable {
         prefs = new XSharedPreferences("lantian.nolitter");
         prefs.makeWorldReadable();
+        String banned = prefs.getString("banned", "");
+        if (banned.isEmpty()) {
+            XposedBridge.log("[NoLitter] Failed to load config, running with defaults");
+        } else {
+            XposedBridge.log("[NoLitter] Config loaded");
+        }
     }
 
     public void handleLoadPackage(final XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
-        XC_MethodHook noLitter = new XC_MethodHook() {
+        XC_MethodHook noLitterStr = new XC_MethodHook() {
             @Override
             protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                if (param.args[0].toString().startsWith("/lantian")) return;
                 String path = param.args[0].toString();
                 if(path.startsWith("/data/")) return;
                 if(path.startsWith("/system/")) return;
@@ -59,10 +67,68 @@ public class XposedHook implements IXposedHookZygoteInit, IXposedHookLoadPackage
                 }
             }
         };
+        XC_MethodHook noLitterStrStr = new XC_MethodHook() {
+            @Override
+            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                if (param.args[0] == null || param.args[0].toString().isEmpty()) {
+                    String path = param.args[1].toString();
+                    if (path.startsWith("/data/")) return;
+                    if (path.startsWith("/system/")) return;
+                    if (path.startsWith("/cache/")) return;
+                    String newPath = doReplace(path);
+                    if (!path.equals(newPath)) {
+                        param.args[1] = newPath;
+                        //XposedBridge.log("[NoLitter] " + lpparam.packageName + ": Redirecting " + path + " -> " + newPath);
+                    }
+                } else {
+                    String path = param.args[0].toString() + "/" + param.args[1].toString();
+                    path = path.replace("//", "/");
+                    if (path.startsWith("/data/")) return;
+                    if (path.startsWith("/system/")) return;
+                    if (path.startsWith("/cache/")) return;
+                    String newPath = doReplace(path);
+                    if (!path.equals(newPath)) {
+                        param.args[0] = null;
+                        param.args[1] = newPath;
+                        //XposedBridge.log("[NoLitter] " + lpparam.packageName + ": Redirecting " + path + " -> " + newPath);
+                    }
+                }
+            }
+        };
+        XC_MethodHook noLitterFileStr = new XC_MethodHook() {
+            @Override
+            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                if (param.args[0] == null || ((File) param.args[0]).getAbsolutePath().isEmpty()) {
+                    String path = param.args[1].toString();
+                    if (path.startsWith("/data/")) return;
+                    if (path.startsWith("/system/")) return;
+                    if (path.startsWith("/cache/")) return;
+                    String newPath = doReplace(path);
+                    if (!path.equals(newPath)) {
+                        param.args[1] = newPath;
+                        //XposedBridge.log("[NoLitter] " + lpparam.packageName + ": Redirecting " + path + " -> " + newPath);
+                    }
+                } else {
+                    String path = ((File) param.args[0]).getAbsolutePath() + "/" + param.args[1].toString();
+                    path = path.replace("//", "/");
+                    if (path.startsWith("/data/")) return;
+                    if (path.startsWith("/system/")) return;
+                    if (path.startsWith("/cache/")) return;
+                    String newPath = doReplace(path);
+                    if (!path.equals(newPath)) {
+                        param.args[0] = null;
+                        param.args[1] = newPath;
+                        //XposedBridge.log("[NoLitter] " + lpparam.packageName + ": Redirecting " + path + " -> " + newPath);
+                    }
+                }
+            }
+        };
         prefs.reload();
         ArrayList<String> banned = new ArrayList<>(Arrays.asList(prefs.getString("banned", "pl.solidexplorer2,com.mixplorer,com.cyanogenmod.filemanager,nextapp.fx,pl.mkexplorer.kormateusz,com.lonelycatgames.Xplore,bin.mt,com.estrongs.android.pop,com.speedsoftware.rootexplorer,bin.mt.plus").split(",")));
         if (!(lpparam.packageName.equals("lantian.nolitter") || banned.contains(lpparam.packageName))) {
-            XposedHelpers.findAndHookConstructor(File.class, String.class, noLitter);
+            XposedHelpers.findAndHookConstructor(File.class, String.class, noLitterStr);
+            XposedHelpers.findAndHookConstructor(File.class, String.class, String.class, noLitterStrStr);
+            XposedHelpers.findAndHookConstructor(File.class, File.class, String.class, noLitterFileStr);
         }
     }
 
@@ -81,8 +147,15 @@ public class XposedHook implements IXposedHookZygoteInit, IXposedHookLoadPackage
                 if(path.startsWith(storageDir + "/Android")) return path;
                 // Split out path after storage dir
                 String newPath = path.substring(storageDir.length() + 1, path.length());
-                File f = new File(URI.create("file://" + urlEncode(storageDir + "/" + newPath.split("/")[0])));
-                if(f.exists()) {
+
+                // File to URI: Create a mock file, get its URI, and replace the mock part
+                File fPath = new File("/lantian" + storageDir + "/" + newPath.split("/")[0]);
+                File fURI = new File(URI.create(fPath.toURI().toString().replace("file:/lantian/", "file:/")));
+                //XposedBridge.log(fPath.toURI().toString());
+
+                // Old method: does not support Chinese
+                //File fURI = new File(URI.create("file://" + urlEncode(storageDir + "/" + newPath.split("/")[0])));
+                if (fURI.exists()) {
                     return path;
                 } else {
                     return storageDir + "/Android/files/" + newPath;
